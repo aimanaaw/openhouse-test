3e95be10a16e998be7f3f9b6499df4f98e4080d9
Aiman <aimanaaw@gmail.com>
2020-08-22 20:24:54 -0400

    Modifications

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask
-app = Flask(__name__)
+# app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -13,3 +16,4 @@ def user(name):
 
 if __name__ == '__main__':
     app.run()
+
